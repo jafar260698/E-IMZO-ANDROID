42e1970eceb5cf28a88812df249f35811cdfc4d9
j.temirov <j.temirov@yt.uz>
2021-09-10 07:45:44 +0500

    - ui fixed

--- app/src/main/java/com/example/myapplication/api/ApiClient.kt
+++ app/src/main/java/com/example/myapplication/api/ApiClient.kt
@@ -1,11 +1,28 @@
 package com.example.myapplication.api
 
+import com.example.myapplication.model.DeepLinkResponse
+import com.example.myapplication.model.StatusModel
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.POST
+import retrofit2.http.Query
+import retrofit2.http.Url
 
-class ApiClient {
+interface ApiClient {
+
+    @POST
+    suspend fun getDeepLink(@Url url: String): Response<DeepLinkResponse>
+
+    @POST
+    suspend fun checkStatus(
+        @Query("documentId")
+        documentId: String,
+        @Url url: String,
+    ): Response<StatusModel>
+
 
     companion object {
         var BASE_URL = "https://my.soliq.uz/"
